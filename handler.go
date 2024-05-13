9a135265eabcc4980c0a4f78777281d86f436c9c
m15h4nya <i.am.ganter@gmail.com>
2024-05-13 13:00:53 +0300

    swagger

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -25,27 +26,50 @@ func NewHandler() Handle {
 	}
 }
 
+func writeMarshal(w http.ResponseWriter, data any, status int) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+		status = http.StatusInternalServerError
+	}
+	w.WriteHeader(status)
+	_, err = w.Write(bytes)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// @Summary	All files existing in vault
+// @Produce	json
+// @Success	200	{array}	string
+// @Failure	500
+// @Router	/api/all_files [get]
 func (h Handle) AllFiles(w http.ResponseWriter, r *http.Request) {
 	content, err := h.vault.AllFiles()
 	if err != nil {
 		fmt.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
+		writeMarshal(w, nil, http.StatusInternalServerError)
 	}
 
-	fmt.Fprint(w, content)
+	writeMarshal(w, content, http.StatusOK)
 }
 
+// @Summary	File's content
+// @Produce	plain
+// @Success	200
+// @Failure	500
+// @Param	filename	path	string	true	"Filename"
+// @Router	/api/file/{filename} [get]
 func (h Handle) FileContent(w http.ResponseWriter, r *http.Request) {
 	filename := mux.Vars(r)["filename"]
 
 	content, err := h.vault.FileContent(filename)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
+		fmt.Println(err)
+		writeMarshal(w, nil, http.StatusInternalServerError)
 	}
 
-	fmt.Fprint(w, content)
+	writeMarshal(w, content, http.StatusOK)
 }
 
 func (h Handle) EditFile(w http.ResponseWriter, r *http.Request) {
--- server.go
+++ server.go
@@ -4,8 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/m15h4nya/online_vault/docs"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
+// @title			Technique
+// @version		1.0
+// @description	vault reader
 func NewServer() http.Server {
 	return http.Server{
 		Handler: router(NewHandler()),
@@ -22,6 +27,8 @@ func router(handler Handle) *mux.Router {
 
 	postHandler := r.Methods(http.MethodPost).Subrouter()
 	postHandler.HandleFunc("/api/file/{filename}", handler.EditFile)
+
+	getHandler.PathPrefix("/swagger/").HandlerFunc(httpSwagger.Handler())
 
 	return r
 }
